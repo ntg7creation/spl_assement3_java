ea0213cb2ca2bbe440977a097f8acfe7fdd2dc95
bar <barsidon@gamil.com>
2018-12-26 13:06:31 +0200

    jj

---
+++ src/main/java/bgu/spl/net/messages/Post.java
@@ -0,0 +1,32 @@
+package bgu.spl.net.messages;
+
+import java.nio.charset.StandardCharsets;
+
+public class Post implements Message{ {
+	
+	private String content;
+	
+	public Post(byte[] b) {
+		content = new String(b, 0, b.length, StandardCharsets.UTF_8);
+	}
+
+	public Object get1() {
+		return content;
+	}
+
+	public Object get2() {
+		throw new IllegalArgumentException("Post messege dosent have a 2'th element");
+	}
+
+	public Object get3() {
+		throw new IllegalArgumentException("Post messege dosent have a 3'th element");
+	}
+
+	public Object get4() {
+		throw new IllegalArgumentException("Post messege dosent have a 4'th element");
+	}
+
+	public Object get5() {
+		throw new IllegalArgumentException("Post messege dosent have a 5'th element");
+	}
+} 
