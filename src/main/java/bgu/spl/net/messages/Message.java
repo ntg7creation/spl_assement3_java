fab939bf8b708239a68ada55389564d2ca9d535b
bar <barsidon@gamil.com>
2018-12-26 12:57:44 +0200

    hh

---
+++ src/main/java/bgu/spl/net/messages/Message.java
@@ -0,0 +1,15 @@
+package bgu.spl.net.messages;
+
+public interface Message {
+	
+
+	
+	
+	public Object get1();
+	public Object get2();
+	public Object get3();
+	public Object get4();
+	public Object get5();
+	
+
+}
---
+++ src/main/java/bgu/spl/net/messages/Register.java
@@ -0,0 +1,75 @@
+package bgu.spl.net.messages;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class Register implements Message{
+	
+	private String username;
+	private String password;
+	private byte[] bytesOfUserName = new byte[1 << 10]; //start with 1k
+	private byte[] bytesOfPassword = new byte[1 << 10]; //start with 1k
+    private int len = 0;
+
+	
+	public Register(byte[] b) {
+		int counterOfElemnts = 0;
+		for (byte currByte : b) {
+			if (counterOfElemnts == 0) {
+				if (currByte == '\0') {
+					counterOfElemnts++;
+					username = new String(bytesOfUserName, 0, len, StandardCharsets.UTF_8);
+					len =0;
+				}
+				pushByteOfUserName(currByte);
+			}
+			else {
+				if (currByte == '\0') {
+					password = new String(bytesOfPassword, 0, len, StandardCharsets.UTF_8);
+				}
+				pushByteOfPassword(currByte);
+			}
+		}
+	}
+	
+
+	public String get1() {
+		return username;
+	}
+	
+	public String get2() {
+		return password;
+	}
+	
+	public Object get3() {
+		throw new IllegalArgumentException("Register messege dosent have a 3'th element");
+	}
+	
+	public Object get4() {
+		throw new IllegalArgumentException("Register messege dosent have a 4'th element");
+	}
+	
+	public Object get5() {
+		throw new IllegalArgumentException("Register messege dosent have a 5'th element");
+	}
+
+
+	private void pushByteOfPassword(byte nextByte) {
+		if (len >= bytesOfPassword.length) {
+			bytesOfPassword = Arrays.copyOf(bytesOfPassword, len * 2);
+		}
+
+		bytesOfPassword[len++] = nextByte;
+	}
+
+	private void pushByteOfUserName(byte nextByte) {
+		if (len >= bytesOfUserName.length) {
+			bytesOfUserName = Arrays.copyOf(bytesOfUserName, len * 2);
+		}
+
+		bytesOfUserName[len++] = nextByte;
+	}
+
+
+	
+}
