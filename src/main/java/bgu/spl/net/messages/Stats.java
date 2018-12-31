f54d3552bee24e6f0782caa6381f354098df40e5
natai <ntg7creation@gmail.com>
2018-12-31 13:52:29 +0200

    stats + private message

---
+++ src/main/java/bgu/spl/net/messages/Stats.java
@@ -0,0 +1,52 @@
+package bgu.spl.net.messages;
+
+public class Stats implements MyMessage {
+
+	private String username;
+
+	public Stats(byte[] b) {
+		int start = 0;
+		int end = 0;
+
+		while (end < b.length && b[end] != '\0')
+			end++;
+
+		username = new String(b, start, end);
+	}
+
+	@Override
+	public Object get1() {
+		return username;
+	}
+
+	@Override
+	public Object get2() {
+		throw new IllegalArgumentException("no item 2 in this type of messge (stats)");
+		
+	}
+
+	@Override
+	public Object get3() {
+		throw new IllegalArgumentException("no item 3 in this type of messge (stats)");
+		
+	}
+
+	@Override
+	public Object get4() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object get5() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public byte[] encode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/bgu/spl/net/messages/privateMessage.java
@@ -0,0 +1,58 @@
+package bgu.spl.net.messages;
+
+public class privateMessage implements MyMessage {
+
+	private String  username;
+	private String Content;
+	
+	public  privateMessage(byte[] b) {
+		int start = 0;
+		int end = 0;
+
+		while (end < b.length && b[end] != '\0')
+			end++;
+
+		username = new String(b, start, end);
+		
+		end++;
+		start = end;
+		while(end<b.length && b[end]!='\0')
+			end++;
+		Content = new String(b,start,end);
+	}
+	
+	@Override
+	public Object get1() {
+		return username;
+	}
+
+	@Override
+	public Object get2() {
+		return Content;
+	}
+
+	@Override
+	public Object get3() {
+		throw new IllegalArgumentException("no item 3 in this type of messge (PM)");
+		
+	}
+
+	@Override
+	public Object get4() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object get5() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public byte[] encode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
